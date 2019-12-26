eec4450805a6b37a3dacf5acf7566c6735876a75
shoumen <0showman0@gmail.com>
2019-12-26 14:08:45 +0300

    Create plyr

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,25 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <vue-plyr ref="plyr">
+      <video src="video.mp4"></video>
+    </vue-plyr>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+  export default {
+    data() {
+      return {
+      }
+    },
+    computed: {
+      player() {
+        return this.$refs.plyr.player
+      }
+    },
   }
-}
+</script>
+
+<style lang="sass">
+  @import "./assets/sass/main"
 </style>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VuePlyr from 'vue-plyr'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(VuePlyr, {
+  plyr: {
+    fullscreen: { enabled: false }
+  },
+  emit: ['ended']
+});
 
 new Vue({
   router,
